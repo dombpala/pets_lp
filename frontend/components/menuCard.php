fbf67679d8b90b6ca48503e280fa1797a8b300aa
dombpala <dombpala@users.noreply.github.com>
2021-02-01 19:50:45 -0500

    Changing icons of menu

--- frontend/components/menuCard.php
+++ frontend/components/menuCard.php
@@ -3,7 +3,7 @@
         return ('
             <ul class="grid grid-cols-2 gap-10 m-10">
                 <li>
-                    <a href="../pages/donaciones.php" class="bg-gray-50 hover:bg-purple-300 hover:border-transparent hover:shadow-lg group block rounded-lg p-4 border border-gray-200">
+                    <a href="../assets/donaciones.svg" class="bg-gray-50 hover:bg-purple-300 hover:border-transparent hover:shadow-lg group block rounded-lg p-4 border border-gray-200">
                         <img class="rounded-full w-40 h-40 mx-auto"" src="../resources/donaciones.jpg" alt="">
                         <h1 class="text-xl text-center font-bold">Donaciones</h1>  
                     </a>
