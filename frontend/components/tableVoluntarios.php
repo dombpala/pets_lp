b04a2bc8abb73a9df4f9b0a2d04ca021a9f70941
dombpala <dombpala@users.noreply.github.com>
2021-02-02 20:00:23 -0500

    Linking pages Voluntarios and Mascotas with backend

--- frontend/components/tableVoluntarios.php
+++ frontend/components/tableVoluntarios.php
@@ -1,41 +1,37 @@
 <?php
+
+    function tileVoluntario($props=[]){
+        $date = date_create($props["fechaNacimiento"]);
+        $date = date_format($date, 'Y-m-d');
+        return(
+            '<div class="grid grid-cols-5">
+                <div class="font-medium text-center bg-gray-300 py-4">'.$props["cedula"].'</div>
+                <div class="font-medium text-center bg-gray-300 py-4">'.$props["nombre"].'</div>
+                <div class="font-medium text-center bg-gray-300 py-4">'.$props["apellido"].'</div>
+                <div class="font-medium text-center bg-gray-300 py-4">'.$date.'</div>
+                <div class="font-medium text-center bg-gray-300 py-4">'.$props["tipo"].'</div>
+            </div>'
+        );
+    }
+
     function tableVoluntarios ($props=[]){
-        return <<<HTML
-
-        <div class="grid grid-cols-5 gap-y-5 my-10 ml-10 ">
-            <div class="flex flex-col">
-                <div class="text-3xl text-center font-medium text-gray-800 mr-6">2</div>
-                <div class="text-3xl text-center font-medium text-gray-800 mr-6">Voluntarios</div>
-            </div>
-            <div class="flex flex-col col-span-4 gap-y-5">
-                <div class="grid grid-cols-5">
-                    <div class="font-extrabold text-center text-lg py-2 bg-gray-100">Cédula</div>
-                    <div class="font-extrabold text-center text-lg py-2 bg-gray-100">Nombre</div>
-                    <div class="font-extrabold text-center text-lg py-2 bg-gray-100">Apellido</div>
-                    <div class="font-extrabold text-center text-lg py-2 bg-gray-100">Fecha de Nacimiento</div>
-                    <div class="font-extrabold text-center text-lg py-2 bg-gray-100">Tipo</div>
+        return(
+            '<div class="grid grid-cols-5 gap-y-5 my-10 ml-10 ">
+                <div class="flex flex-col">
+                    <div class="text-3xl text-center font-medium text-gray-800 mr-6">'.count($props).'</div>
+                    <div class="text-3xl text-center font-medium text-gray-800 mr-6">Voluntarios</div>
                 </div>
-
-                <div class="grid grid-cols-5">
-                    <div class="font-medium text-center bg-gray-300 py-4">0955647382</div>
-                    <div class="font-medium text-center bg-gray-300 py-4">Luis</div>
-                    <div class="font-medium text-center bg-gray-300 py-4">Cajas</div>
-                    <div class="font-medium text-center bg-gray-300 py-4">02/03/1993</div>
-                    <div class="font-medium text-center bg-gray-300 py-4">Veterinario</div>
+                <div class="flex flex-col col-span-4 gap-y-5">
+                    <div class="grid grid-cols-5">
+                        <div class="font-extrabold text-center text-lg py-2 bg-gray-100">Cédula</div>
+                        <div class="font-extrabold text-center text-lg py-2 bg-gray-100">Nombre</div>
+                        <div class="font-extrabold text-center text-lg py-2 bg-gray-100">Apellido</div>
+                        <div class="font-extrabold text-center text-lg py-2 bg-gray-100">Fecha de Nacimiento</div>
+                        <div class="font-extrabold text-center text-lg py-2 bg-gray-100">Tipo</div>
+                    </div>
+                    '.implode(" ",array_map("tileVoluntario",$props)).'
                 </div>
-
-                <div class="grid grid-cols-5">
-                    <div class="font-medium text-center bg-gray-300 py-4">0967123312</div>
-                    <div class="font-medium text-center bg-gray-300 py-4">Hugo</div>
-                    <div class="font-medium text-center bg-gray-300 py-4">Vásquez</div>
-                    <div class="font-medium text-center bg-gray-300 py-4">06/08/1996</div>
-                    <div class="font-medium text-center bg-gray-300 py-4">Cuidador</div>
-                </div>
-
-            </div>
-
-        </div>
-
-        HTML;
+            </div>' 
+        );
     }
 ?>
